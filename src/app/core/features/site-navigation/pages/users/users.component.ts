8ce2b5d507874b9cc079efef9890a3ef6540eeb6
gDaddy <g3orrg301@gmail.com>
2023-09-21 20:42:45 +0300

    dummy json server configuration

--- src/app/core/features/site-navigation/pages/users/users.component.ts
+++ src/app/core/features/site-navigation/pages/users/users.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent {
+  ngOnInit() {
+    console.log('george')
+    fetch('http://localhost:3000/posts')
+      .then(res => res.json())
+      .then(res => console.log(res));
 
+    fetch('http://localhost:3000/dogs', {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ id: "22", "title": "GGGG", status: "fffff" })
+    })
+      .then(res => console.log(res))
+  }
 }
